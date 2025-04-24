3aa1ef3c0da11d4698d4319139ee61f11ab1185b
MaiQuocKhang1998 <khang1998k@gmail.com>
2025-04-24 09:08:50 +0700

    Add files via upload

---
+++ scr/Default/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            FestivalRepeater.DataSource = Festival.GetFestivals();
+            FestivalRepeater.DataBind();
+        }
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Login.aspx");
+    }
+
+    protected void btnRegister_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Register.aspx");
+    }
+}
