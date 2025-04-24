6a12ee32c036982ec3539fa0465bace17dabbbfe
MaiQuocKhang1998 <khang1998k@gmail.com>
2025-04-24 09:10:54 +0700

    Add files via upload

---
+++ scr/FestivalDetails/FestivalDetails.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+
+public partial class FestivalDetails : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack && Request.QueryString["id"] != null)
+        {
+            int id;
+            if (int.TryParse(Request.QueryString["id"], out id))
+            {
+                var festival = Festival.GetFestivalById(id);
+                if (festival != null)
+                {
+                    lblName.Text = festival.Name;
+                    lblDescription.Text = festival.Description;
+                    imgFestival.Src = festival.ImageUrl;
+                    FestivalPanel.Visible = true;
+                }
+                else
+                {
+                    lblError.Text = "Không tìm thấy lễ hội.";
+                    lblError.Visible = true;
+                }
+            }
+        }
+    }
+}
