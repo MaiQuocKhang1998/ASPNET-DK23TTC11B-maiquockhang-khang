51552d2f715425f9d53fc6a1ba0815a7d374babe
MaiQuocKhang1998 <khang1998k@gmail.com>
2025-04-25 21:13:16 +0700

    Add files via upload

---
+++ scr/Welcome.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public partial class Welcome : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["username"] != null)
+            lblUsername.Text = Session["username"].ToString();
+        else
+            Response.Redirect("Login.aspx");
+    }
+}
